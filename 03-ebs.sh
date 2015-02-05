9b545ff0b7971822c364f97b8885c94f6d450108
Jeremy Archer <open-source@fatlotus.com>
2015-02-05 17:07:07 -0600

    Correct for xvdb.

--- 03-ebs.sh
+++ 03-ebs.sh
@@ -2,17 +2,17 @@
 #
 # This script adds support for the EBS mount at /home.
 # 
-# It is expected that /dev/sdb is already formatted as a ext3 volume.
+# It is expected that /dev/xvdb is already formatted as a ext3 volume.
 #
 
-# Configure /dev/sdb as an ext3 volume.
-sudo sed -i ':/dev/sdb:d' /etc/fstab
+# Configure /dev/xvdb as an ext3 volume.
+sudo sed -i ':/dev/xvdb:d' /etc/fstab
 
 sudo tee -a /etc/fstab <<EOF
-/dev/sdb	/home	ext3	defaults	0 0
+/dev/xvdb	/home	ext3	defaults	0 0
 EOF
 
 # Mount the volume as /home.
-if mount | grep -v /dev/sdb; then
+if mount | grep -v /dev/xvdb; then
 	sudo mount -a
 fi
