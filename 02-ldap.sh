0c82b03f2c2aa87be969f5df135f6b3bfa0715c2
Jeremy Archer <open-source@fatlotus.com>
2015-02-05 17:12:04 -0600

    Add restart after SSHD update.

--- 02-ldap.sh
+++ 02-ldap.sh
@@ -25,3 +25,5 @@ sudo tee -a /etc/ssh/sshd_config > /dev/null <<EOF
 Match User jarcher,lafferty,vvora,yjchoe # LSDASETUP
     PasswordAuthentication yes # LSDASETUP
 EOF
+
+sudo /etc/init.d/ssh restart
