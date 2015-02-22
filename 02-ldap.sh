9c7361b852aac5422968b905e0dfb82d1e182741
Jeremy Archer <open-source@fatlotus.com>
2015-02-22 15:10:05 -0600

    Add LDAP remapping script.

--- 02-ldap.sh
+++ 02-ldap.sh
@@ -16,11 +16,23 @@ Session:
         required                        pam_mkhomedir.so umask=0022 skel=/etc/skel
 EOF
 
+# Remap attributes for users.
+sudo sed -i '/LSDASETUP/d' /etc/ldap.conf
+sudo tee /etc/lib.conf > /dev/null <<EOF
+uid nslcd
+gid nslcd
+uri ldap://ldap.uchicago.edu
+base dc=uchicago,dc=edu
+ssl start_tls
+tls_reqcert never
+map passwd loginShell "/bin/bash"
+map passwd homeDirectory "/home/$uid"
+EOF
+
 sudo /etc/init.d/nscd restart
 
 # Update SSH to allow remote student logins.
 sudo sed -i '/LSDASETUP/d' /etc/ssh/sshd_config
-
 sudo tee -a /etc/ssh/sshd_config > /dev/null <<EOF
 Match User jarcher,lafferty,vvora,yjchoe # LSDASETUP
     PasswordAuthentication yes # LSDASETUP
